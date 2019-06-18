23808eb449597ac0da1db89c629ea2808247a59c
unknown <ch04937>
2019-06-18 13:07:12 -0700

    used mouseover to change the color of 'fun bus' to red

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+//using mouseover to change the color of 'fun bus'
+logo = document.getElementsByClassName('logo-heading')
+logo[0].addEventListener('mouseover', mouseOver = () => {
+    logo[0].style.color = 'red';
+});
+logo[0].addEventListener('mouseout', mouseOut = () => {
+    logo[0].style.color = 'black';
+});
+
+
